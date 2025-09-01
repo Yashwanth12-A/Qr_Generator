2b2f020acb36376e266bedfa149c0ca6ff71cf2f
Yash <ayashwanth@1124gmail.com>
2025-09-01 23:10:32 +0530

    ignored node_modules

---
+++ 2.4 QR Code Project/index.js
@@ -0,0 +1,40 @@
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "url",
+      message: "Please enter the URL you want to convert to QR code:",
+    },
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url = answers.url;
+
+    var qr_png = qr.image(url, { type: "png" });
+    qr_png.pipe(fs.createWriteStream("url.png"));
+    var png_string = qr.imageSync(url, { type: "png" }); // Optional: Get PNG as buffer
+
+    //saving url to a text file
+    fs.writeFile("url.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The Url has been written to url.txt file");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+      console.error("Prompt couldn't be rendered in the current environment");
+    } else {
+      // Something else went wrong
+      console.error("Something else went wrong", error);
+    }
+  });
---
+++ README.md
@@ -0,0 +1,25 @@
+# 🔗 URL to QR Code Generator
+
+This Node.js CLI tool allows users to:
+
+1. Input a URL using the `inquirer` package.
+2. Generate a QR code image from the URL using `qr-image`.
+3. Save the entered URL to a `.txt` file using Node's native `fs` module.
+
+---
+
+## 🚀 Features
+
+- Interactive command-line prompt for URL input
+- PNG QR code generation
+- URL logging to `url.txt` for record-keeping
+
+---
+
+## 📦 Dependencies
+
+Make sure you have the following packages installed:
+
+```bash
+npm install inquirer qr-image
+```
